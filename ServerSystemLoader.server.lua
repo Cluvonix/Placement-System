0a0d3212981f53c0aad4b9fc0d6f4215bcf9d3b6
Cluvonix <jacksonlein@icloud.com>
2025-11-13 18:13:52 -0600

    Create ServerSystemLoader.server.lua

---
+++ ServerSystemLoader.server.lua
@@ -0,0 +1,6 @@
+local ServerScriptService = game:GetService("ServerScriptService")
+
+local PlacementHandler = require(ServerScriptService.Systems.PlacementSystem.PlacementHandler)
+local PlotManager = require(ServerScriptService.Systems.PlotSystem.PlotManager)
+
+PlotManager.Init()
